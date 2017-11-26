aa3e5ac4d2e3041f40c88b846e2c8efed3b8ec95
Santiago Barroso <sbarroso.dev@gmail.com>
2017-11-26 22:44:21 +0100

    Prepare project to be used in angular

--- stencil.config.js
+++ stencil.config.js
@@ -1,10 +1,10 @@
 exports.config = {
   bundles: [
-    { components: ['progress-bar', 'my-slider'] }
+    { components: ['progress-bar'] },
+    { components: ['my-slider'] }
   ],
-  collections: [
-    { name: '@stencil/router' }
-  ]
+  buildDir: 'stencil-components/build',
+  collections: [ ]
 };
 
 exports.devServer = {
